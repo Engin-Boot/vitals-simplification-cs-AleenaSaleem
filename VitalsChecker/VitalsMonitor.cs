a6db98c42c7237af0e88ce5b96787a0786b0ed61
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-08 21:28:29 +0530

    update

--- VitalsChecker/VitalsMonitor.cs
+++ VitalsChecker/VitalsMonitor.cs
@@ -16,19 +16,20 @@ namespace VitalsChecker
             
             CultureInfo provider = new CultureInfo("en-us");
             bool status = true;
-            if (one_vital.getValue()<one_vital.getLower())
+            float value = one_vital.getValue();
+            if (value < one_vital.getLower())
             {
                 message += one_vital.getName();
                 message += " Very Low with value: ";
-                message += one_vital.getValue().ToString(provider);
+                message += value.ToString(provider);
                 message += "\n";
                 status = false;
             }
-            if(one_vital.getValue() > one_vital.getUpper())
+            if(value  > one_vital.getUpper())
             {
                 message += one_vital.getName();
                 message += " Very High with value: ";
-                message += one_vital.getValue().ToString(provider);
+                message += value.ToString(provider);
                 message += "\n";
                 status = false;
                 
