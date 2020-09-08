01e0de5235f1ea3f27badb8c85cd517fbb3fb137
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-08 20:20:11 +0530

    Add more tests

--- VitalsChecker/Program.cs
+++ VitalsChecker/Program.cs
@@ -8,14 +8,14 @@ namespace VitalsChecker
 {
     public class VitalsCollection
     {
-        public  Vitals[] allVitals;
+        public Vitals[] allVitals;
         public VitalsCollection(int n_vitals)
         {
             allVitals = new Vitals[n_vitals];
         }
         public void Initialize()
         {
-            for (int i = 0; i < allVitals.Length;i++)
+            for (int i = 0; i < allVitals.Length; i++)
             {
                 allVitals[i] = new Vitals();
                 allVitals[i].Initialize();
@@ -37,17 +37,31 @@ namespace VitalsChecker
         static void Main(string[] args)
         {
             VitalsCollection v_collection = new VitalsCollection(3);
+            Vitals one_vital = new Vitals();
+            one_vital.setName("Test1");
+            one_vital.setValue(30);
+            one_vital.setLower(25);
+            one_vital.setUpper(50);
             v_collection.Initialize();
             v_collection.UpdateVitals();
             VitalsMonitor v_monitor = new VitalsMonitor();
             AlertInSMS alertSMS = new AlertInSMS();
             AlertInIntercom alertIntercom = new AlertInIntercom();
 
-            Checker.ExpectTrue(v_monitor.VitalsAreNormal(alertSMS,v_collection));
-            v_collection.UpdateVitals();
-            Checker.ExpectFalse(v_monitor.VitalsAreNormal(alertIntercom,v_collection));
+
+            //Testing VitalsAreNormal()
+            Checker.ExpectTrue(v_monitor.VitalsAreNormal(alertSMS, v_collection));
             v_collection.UpdateVitals();
             Checker.ExpectFalse(v_monitor.VitalsAreNormal(alertIntercom, v_collection));
+            v_collection.UpdateVitals();
+            Checker.ExpectFalse(v_monitor.VitalsAreNormal(alertIntercom, v_collection));
+
+            //Testing VitalIsNormal()
+            Checker.ExpectTrue(v_monitor.VitalIsNormal(one_vital));
+            one_vital.setValue(20);
+            Checker.ExpectFalse(v_monitor.VitalIsNormal(one_vital));
+            one_vital.setValue(56);
+            Checker.ExpectFalse(v_monitor.VitalIsNormal(one_vital));
             Console.WriteLine("All ok");
         }
     }
